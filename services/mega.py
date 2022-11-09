99d82a2dca89d9b6920e6cd48e4ca08cbd7c5cf3
MightyDuke <papa.the.roach@gmail.com>
2022-11-09 13:03:58 +0100

    Mega improvements

--- services/mega.py
+++ services/mega.py
@@ -1,32 +1,31 @@
 import asyncio
 import tempfile
 import os
-import uuid
 from contextlib import asynccontextmanager
 from mastobot import Service
 
 class Mega(Service):
     @property
     def __common_arguments(self):
-        return "-u", self.username, "-p", self.password, "--reload"
+        return "-u", self.username, "-p", self.password
 
     async def ls(self, path):
         process = await asyncio.create_subprocess_exec(
             "megals", *self.__common_arguments, 
-            f"/Root{path}",
+            os.path.join("/Root", path.removeprefix("/")),
             stdout=asyncio.subprocess.PIPE
         )
-        stdout, stderr = await process.communicate()
-        return [file[5:] for file in stdout.decode().splitlines()]
+        stdout, _ = await process.communicate()
+        return [file.removeprefix("/Root") for file in stdout.decode().splitlines()]
 
     @asynccontextmanager
     async def get(self, path):
-        file_name = os.path.join(tempfile.gettempdir(), uuid.uuid4().hex)
+        file_name = tempfile.mktemp()
 
         process = await asyncio.create_subprocess_exec(
             "megaget", *self.__common_arguments,
             "--path", file_name,
-            f"/Root{path}",
+            os.path.join("/Root", path.removeprefix("/")),
             stdout=asyncio.subprocess.PIPE
         )
 
